37e1f2401d4ba2aa5ee4c88466bd59dcfddc3c84
HazemAhmed2002 <ahmedhazeml734@gmail.com>
2023-03-04 21:37:28 +0200

    edit now

--- app/src/main/java/com/hzmahmed/assignment_1/MainActivity.kt
+++ app/src/main/java/com/hzmahmed/assignment_1/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.hzmahmed.assignment_1.databinding.ActivityMainBinding
